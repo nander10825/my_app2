e0aa34f3eb45de5e56a54c535ce5766626f727c5
nander10825 <24_10825@daejin.sen.hs.kr>
2025-01-08 11:26:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ import streamlit as st
 st.title("의미없는 버튼들")
 
 # 버튼
-if st.button("버튼"):
+if st.button("버튼1"):
     st.write("그냥 버튼이다 뭐")
 
-if st.button("버튼"):
+if st.button("버튼2"):
     st.write("그냥 버튼이다 뭐")
 
-if st.button("버튼"):
+if st.button("버튼3"):
     st.write("그냥 버튼이다 뭐")
 
-if st.button("버튼"):
+if st.button("버튼4"):
     st.write("그냥 버튼이다 뭐")
 
-if st.button("버튼"):
+if st.button("버튼5"):
     st.write("그냥 버튼이다 뭐")
 
 
