94ea90fd640e8cd6142d96515b2aee7ca5f25835
nander10825 <24_10825@daejin.sen.hs.kr>
2025-01-08 11:20:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,9 @@ st.text("\n\n")
 st.write(" 난 신이다 🪽")
 st.write("내 주소? 알아서 뭐하게!끄져!")
 
-st.button("Reset", type="primary")
-if st.button("Say hello"):
-    st.write("Why hello there")
+st.button("리셋", type="primary")
+if st.button("버튼"):
+    st.write("그냥 버튼이다 뭐")
 else:
     st.write("Goodbye")
 
-if st.button("Aloha", type="tertiary"):
-    st.write("Ciao")
