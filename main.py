d452ac1e92759893b07d3eea40e3e71fcd02c31b
nander10825 <24_10825@daejin.sen.hs.kr>
2025-01-08 11:18:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import streamlit as st
 
 st.title("앱 만들거임")
-
+st.text("\n\n")
 st.write(" 난 신이다 🪽")
 st.write("내 주소? 알아서 뭐하게!끄져!")
+
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
+
+if st.button("Aloha", type="tertiary"):
+    st.write("Ciao")
